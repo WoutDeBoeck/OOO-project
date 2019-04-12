069394d68fafa31ea660a5c4e5512c8fbc7c2f68
WoutDeBoeck <wout.deboeck@student.ucll.be>
2019-04-12 17:54:58 +0200

    KassaTab Finish Button + Visual Tweaks

--- src/view/tab/KassaTab.java
+++ src/view/tab/KassaTab.java
@@ -1,8 +1,12 @@
 package view.tab;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import model.event.EnterKeyHandler;
 import view.tab.table.ArtikelTable;
@@ -10,8 +14,11 @@ import view.tab.table.ArtikelTable;
 public class KassaTab extends Tab
 {
     private TextField input;
+    private Button finish;
 
-    protected VBox mainBox;
+    private VBox mainBox;
+    private HBox controlBox;
+
     private ArtikelTable artikelLijst;
 
 
@@ -22,14 +29,24 @@ public class KassaTab extends Tab
         this.setClosable(false);
 
         input = new TextField();
-        mainBox = new VBox();
+        finish = new Button("Finish");
+
+        mainBox = new VBox(2);
+        controlBox = new HBox(2);
+
+        controlBox.setAlignment(Pos.CENTER);
+
         artikelLijst = new ArtikelTable();
 
         EnterKeyHandler enterHandler = new EnterKeyHandler(artikelLijst);
 
         input.addEventFilter(KeyEvent.KEY_PRESSED, enterHandler);
 
-        mainBox.getChildren().addAll(artikelLijst, input);
+        input.setPrefWidth(696);
+
+        controlBox.getChildren().addAll(input, finish);
+        mainBox.getChildren().addAll(artikelLijst, controlBox);
+
         this.setContent(mainBox);
     }
 }
